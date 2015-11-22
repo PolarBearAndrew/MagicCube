b14be4fd1bdf5da8a0310b07d8de715436b0d08b
PolarBearAndrew <PolarBearAndrew@users.noreply.github.com>
2015-11-22 21:17:06 +0800

    restart

---
+++ devServer/public/asset/javascripts/cube.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+//   setTimeout(todo, 200);
+//
+//   function todo(){
+//     var arr = $('.z3');
+//     for( var i = 0; i < arr.length; i++ ){
+//       elem = $(arr[i]);
+//       $(elem).animate({
+//         transform: '+=rotate(90deg)'
+//       });
+//     } // for end
+//   } // todo end
+// }); // doc ready end
