338b30ddf4e8acca0534952acd0083a4e7dc5495
AndrewChen <PolarBearAndrew@users.noreply.github.com>
2015-12-09 15:11:26 +0800

    stage

--- devServer/public/asset/javascripts/cube.js
+++ devServer/public/asset/javascripts/cube.js
@@ -5,9 +5,7 @@ var options = {
   z: [ -50, 0, 50 ],
 };
 
-var _MAPS = [
-
-];
+var _MAPS = {};
 
 var data = initTransformData(options);
 
@@ -20,19 +18,34 @@ $(document).ready(function(){
     var ry = $(this).attr('data-ry') || 0; // rotateY
     var rz = $(this).attr('data-rz') || 0; // rotateY
 
+    var targets = [];
+    Object.keys(_MAPS).forEach(function(x){
+      Object.keys(_MAPS[x]).forEach(function(y){
+        Object.keys(_MAPS[x][y]).forEach(function(z){
+          if( x == layer || y == layer || z == layer){
+            targets.push(_MAPS[x][y][z]);
+          }
+        });
+      });
+    });
+    console.log('targets', targets, _MAPS['x1']);
+
     var cubes = data.map(function(cube){
       data.indexOf(cube);
-      if(cube.index.indexOf(layer) != -1){
+      // if(cube.index.indexOf(layer) != -1){
+      if(targets.indexOf(cube.classStr) != -1){
         return {
           index: data.indexOf(cube),
-          classStr: cube.classStr,
+          classStr: cube.classStr, // TODO: 有空要換成id
         };
       }
       return false;
     }).filter(function(cube){
       return cube;
     });
-    return doTransform(cubes, rx, ry, rz);
+    doTransform(cubes, rx, ry, rz);
+    offsetCube(layer);
+    return false;
   }); // .ctrl[click] end
 }); // doc ready end
 
@@ -60,11 +73,41 @@ function doTransform(cubes, rx, ry, rz){
   return false;
 }
 
+function offsetCube(layer){
+  var tmp =  _.cloneDeep(_MAPS);
+  if(layer.indexOf('x') != -1){
+    tmp[layer]['y1']['z1'] = _MAPS[layer]['y1']['z3'];
+    tmp[layer]['y1']['z2'] = _MAPS[layer]['y2']['z3'];
+    tmp[layer]['y1']['z3'] = _MAPS[layer]['y3']['z3'];
+    tmp[layer]['y2']['z1'] = _MAPS[layer]['y1']['z2'];
+    // tmp[layer]['y2']['z2'] = _MAPS[layer]['y2']['z2']; //center
+    tmp[layer]['y2']['z3'] = _MAPS[layer]['y3']['z2'];
+    tmp[layer]['y3']['z1'] = _MAPS[layer]['y1']['z1'];
+    tmp[layer]['y3']['z2'] = _MAPS[layer]['y2']['z1'];
+    tmp[layer]['y3']['z3'] = _MAPS[layer]['y3']['z1'];
+  }
+  _MAPS = _.cloneDeep(tmp);
+  return false;
+}
+
 function initTransformData(options){
   var data = [];
+
   options.x.forEach(function(x, xi){
+
+    var xKey = 'x' + (xi + 1);
+    if(!_MAPS[xKey]) _MAPS[xKey] = {};
+
     options.y.forEach(function(y, yi){
+
+      var yKey = 'y' + (yi + 1);
+      if(!_MAPS[xKey][yKey]) _MAPS[xKey][yKey] = {};
+
       options.z.forEach(function(z, zi){
+
+        var zKey = 'z' + (zi + 1);
+        _MAPS[xKey][yKey][zKey] = buildStr(xi, yi, zi, '.');
+
         var cube = {
           x: x,
           y: y,
@@ -72,13 +115,13 @@ function initTransformData(options){
           rx: 0,
           ry: 0,
           rz: 0,
+          xi : xi,
+          yi : yi,
+          zi : zi,
         };
-        cube.index = buildStr(xi, yi, zi)
-        cube.classStr = buildStr(xi, yi, zi, '.')
+        cube.index = buildStr(xi, yi, zi);
+        cube.classStr = buildStr(xi, yi, zi, '.');
         data.push(cube);
-        if( xi == 0 && yi == 0 && zi == 2 ){
-          console.log(cube);
-        }
       });
     });
   });
